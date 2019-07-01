4a712a68f03149bb733e3bd132f4f99d2ce320e2
Tom Westerhout <t.westerhout@student.ru.nl>
2019-07-01 00:29:56 -0400

    Sphere test function

--- test/test_lbfgs.cpp
+++ test/test_lbfgs.cpp
@@ -28,10 +28,40 @@ TEST_CASE("Test function 1", "[lbfgs]")
         ::LBFGS_NAMESPACE::lbfgs_param_t params;
         auto const                       r =
             ::LBFGS_NAMESPACE::minimize(value_and_gradient, params, x0);
-        std::cout << make_error_code(r.status).message() << '\n';
         REQUIRE(r.status == ::LBFGS_NAMESPACE::status_t::success);
         REQUIRE(r.func == Approx(-16.3750));
+        REQUIRE(r.num_iter <= 7);
         REQUIRE(x0[0] == Approx(2.25f));
         REQUIRE(x0[1] == Approx(-4.75f));
+    }
+}
+
+TEST_CASE("Sphere function", "[lbfgs]")
+{
+    auto const value_and_gradient = [](auto const xs, auto const gs) {
+        static_assert(std::is_same_v<std::remove_const_t<decltype(xs)>,
+                                     gsl::span<float const>>);
+        static_assert(std::is_same_v<std::remove_const_t<decltype(gs)>,
+                                     gsl::span<float>>);
+        auto f_x = 0.0;
+        for (auto i = size_t{0}; i < xs.size(); ++i) {
+            auto const x = static_cast<double>(xs[i]);
+            f_x += x * x;
+            gs[i] = 2.0f * xs[i];
+        }
+        return f_x;
+    };
+
+    {
+        std::array<float, 5> x0 = {1.0f, 1.0f, -8.0f, 1.3f, -0.002f};
+        ::LBFGS_NAMESPACE::lbfgs_param_t params;
+        auto const                       r =
+            ::LBFGS_NAMESPACE::minimize(value_and_gradient, params, x0);
+        REQUIRE(r.status == ::LBFGS_NAMESPACE::status_t::success);
+        REQUIRE(r.func == Approx(0));
+        REQUIRE(r.num_iter <= 2);
+        for (auto const x : x0) {
+            REQUIRE(x == Approx(0.0f));
+        }
     }
 }
